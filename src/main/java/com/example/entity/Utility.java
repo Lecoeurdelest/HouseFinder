72ae738a708ca3d05c4307fccc03a10394435741
NguyenTranDuyVu <vuntd1207@gmail.com>
2023-07-03 23:17:18 +0700

    remove utility icon path

--- src/main/java/com/example/entity/Utility.java
+++ src/main/java/com/example/entity/Utility.java
@@ -22,12 +22,10 @@ public class Utility {
     @Column(name = "utility_name")
     private String name;
     //@Column(name = "icon_path")
-    @Column(name = "icon_path")
-    private String iconPath;
 
-    public Utility(String name, String iconPath) {
+
+    public Utility(String name) {
         this.name = name;
-        this.iconPath = iconPath;
     }
 
     @Override
@@ -35,11 +33,11 @@ public class Utility {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Utility utility = (Utility) o;
-        return id.equals(utility.id) && name.equals(utility.name) && iconPath.equals(utility.iconPath);
+        return id.equals(utility.id) && name.equals(utility.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, iconPath);
+        return Objects.hash(id, name);
     }
 }
