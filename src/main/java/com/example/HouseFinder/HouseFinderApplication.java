2a70389e3288850d0f3a2e17733e537b6bf4cced
Lecoeurdelest <=>
2023-07-03 09:34:15 +0700

    Quy

---
+++ src/main/java/com/example/HouseFinder/HouseFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.HouseFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HouseFinder/HouseFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HouseFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
