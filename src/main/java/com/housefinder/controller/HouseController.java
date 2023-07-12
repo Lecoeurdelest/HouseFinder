b132b77a9747dd07cc8287d0b8a52a736737b5ed
Lecoeurdelest <=>
2023-07-13 05:16:30 +0700

    quy

--- src/main/java/com/housefinder/controller/HouseController.java
+++ src/main/java/com/housefinder/controller/HouseController.java
@@ -2,16 +2,11 @@ package com.housefinder.controller;
 
 import com.housefinder.dto.HouseDto;
 import com.housefinder.entity.House;
-<<<<<<< HEAD
-import com.housefinder.service.HouseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-=======
 import com.housefinder.entity.User;
 import com.housefinder.entity.Utility;
 import com.housefinder.service.HouseService;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 61e2456e9158589e0728d51a11c167dbc63c4940
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +21,11 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/houses")
 public class HouseController {
-<<<<<<< HEAD
 
     @Autowired
     private HouseService houseService;
 
-    @GetMapping("/houselist/{pageNumber}")
+    @GetMapping("houselist/{pageNumber}")
     public String getHouseDto(@PathVariable int pageNumber, Model model) {
         Page<House> housePage = houseService.getHouse(pageNumber);
         List<House> houses = housePage.getContent();
@@ -41,15 +35,6 @@ public class HouseController {
         return "houselist";
     }
 
-
-=======
-    @Autowired
-    private HouseService houseService;
-
-    @GetMapping("/house/{pageNumber}")
-    public HouseDto getHouseDto(@PathVariable int pageNumber){
-        return null;
-    }
 
     @GetMapping(value = "/{id}")
     public String getHouseById(Model model, @PathVariable long id){
@@ -61,5 +46,4 @@ public class HouseController {
         model.addAttribute("owner", owner);
         return "housedetail";
     }
->>>>>>> 61e2456e9158589e0728d51a11c167dbc63c4940
 }
